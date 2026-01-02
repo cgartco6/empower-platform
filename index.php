6830ae257d6a7fa0ff2bb45a668f2fa2b7ed3c46
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-02 13:42:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include "track_visit.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Empower Platform</title>
+<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+<h1>Learn Skills. Build Confidence.</h1>
+<p>Simple, South African, beginner friendly.</p>
+
+<a href="free-course.php">🎁 Start Free Course</a><br><br>
+<a href="courses.php">📘 View Paid Courses</a>
+
+</body>
+</html>
